e7f2f583938906aebdb89444a2efb55820b89a69
dogogh <quadros1919@gmail.com>
2023-01-16 09:27:40 -0300

    teste de contador

--- mask.js
+++ mask.js
@@ -1,7 +1,14 @@
 
+let n=0;
 $(document).ready(function () {
   $("#telefone").mask("(99) 99999-9999");
   $("#cpf").mask("999.999.999-99");
   $("#data_nascimento").mask("99/99/9999");
 });
 
+
+console.log(n);
+function numero(){
+  n++
+  console.log(n);
+};
